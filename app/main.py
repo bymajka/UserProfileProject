75e6f068c2150841348f4f3ad390cd53f9c1e82e
Oleksandr Kovalchuk <anxolerd@outlook.com>
2024-12-03 00:46:29 +0200

    Better init data

--- app/main.py
+++ app/main.py
@@ -7,33 +7,44 @@ from .api import api
 def init():
     from app.models import CreatePostModel, CreateUserModel
     from app.logic import add_like_to_post, create_post, create_user
+    import random
 
     password = "12345678"
-    user_1 = CreateUserModel(username="user_1", password=password, full_name="User 1")
-    user_2 = CreateUserModel(username="user_2", password=password, full_name="User 2")
-    user_3 = CreateUserModel(username="user_3", password=password, full_name=None)
-
-    create_user(user_1)
-    create_user(user_2)
-    create_user(user_3)
-
-    post_1_1 = create_post(user_1.username, CreatePostModel(content="Hello world!"))
-    post_1_2 = create_post(
-        user_1.username, CreatePostModel(content="Hello university!")
-    )
-    post_1_3 = create_post(user_1.username, CreatePostModel(content="Hello Ukraine!"))
-
-    post_2_1 = create_post(user_2.username, CreatePostModel(content="Init bot user"))
-    post_2_2 = create_post(
-        user_2.username, CreatePostModel(content="Initialization failed!")
-    )
-
-    add_like_to_post(post_1_1, user_2.username)
-    add_like_to_post(post_1_3, user_2.username)
-    add_like_to_post(post_1_2, user_1.username)
-    add_like_to_post(post_2_1, user_3.username)
-    add_like_to_post(post_2_2, user_3.username)
-    add_like_to_post(post_1_1, user_3.username)
+    texts = [
+        'Is history repeating itself...?',
+        "Damn, it's hard to wrap presents when you're drunk.",
+        "Don't Look a Gift Horse In The Mouth",
+        "Shot In the Dark",
+        "Let Her Rip",
+        "He excelled at firing people nicely.",
+        "I don’t respect anybody who can’t tell the difference between Pepsi and Coke.",
+        "We have a lot of rain in June.",
+        "I may struggle with geography, but I'm sure I'm somewhere around here."
+        "He wasn't bitter that she had moved on but from the radish.",
+        "Barking dogs and screaming toddlers turn friendly neighbors into cranky enemies."
+        "He uses onomatopoeia as a weapon of mental destruction.",
+        "Every manager should be able to recite at least ten nursery rhymes backward.",
+        "Make a sandwich blindfolded, then take a bite."
+        "You Can't Teach an Old Dog New Tricks",
+        "Off One's Base",
+        "Mountain Out of a Molehill",
+        "There's a message for you if you look up.",
+        "It must be five o'clock somewhere.",
+    ]
+    users = [
+        create_user(CreateUserModel(
+            username=f"user_{i + 1}",
+            password=password,
+            full_name=random.choice([f"User {i + 1}", None]),
+        ))
+        for i in range(3)
+    ]
+    posts = [
+        create_post(random.choice(users).username, CreatePostModel(content=text))
+        for text in texts
+    ]
+    for _ in range(len(texts) * 2):
+        add_like_to_post(random.choice(posts), random.choice(users).username)
 
 
 init()
